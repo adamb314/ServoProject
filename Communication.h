6eb4a1f1093b248630895a24c699a62ef43e9854
adam <adamb3.14@gmail.com>
2019-04-16 16:05:27 +0200

    fixed indentation in Communication.h

--- Communication.h
+++ Communication.h
@@ -7,39 +7,39 @@
 
 class Communication
 {
- public:
-  Communication(unsigned char nodeNr, unsigned long baud = 9600);
+public:
+    Communication(unsigned char nodeNr, unsigned long baud = 9600);
 
-  bool run();
+    bool run();
 
-  bool blockingRun();
+    bool blockingRun();
 
-  int intArray[16];
-  char charArray[8];
+    int intArray[16];
+    char charArray[8];
 
-  bool intArrayChanged[16];
-  bool charArrayChanged[8];
+    bool intArrayChanged[16];
+    bool charArrayChanged[8];
 
- private:
-  int intArrayBuffer[16];
-  char charArrayBuffer[8];
+private:
+    int intArrayBuffer[16];
+    char charArrayBuffer[8];
 
-  bool intArrayChangedBuffer[16];
-  bool charArrayChangedBuffer[8];
+    bool intArrayChangedBuffer[16];
+    bool charArrayChangedBuffer[8];
 
-  int communicationState;
-  unsigned char waitForBytes;
-  unsigned char messageLength;
-  unsigned char command;
-  unsigned char checksum;
-  bool communicationError;
-  unsigned long lastAvailableReadTimestamp;
+    int communicationState;
+    unsigned char waitForBytes;
+    unsigned char messageLength;
+    unsigned char command;
+    unsigned char checksum;
+    bool communicationError;
+    unsigned long lastAvailableReadTimestamp;
 
-  int sendCommunicationState;
-  unsigned char numberOfSendCommands;
-  unsigned char currentSendCommandIndex;
-  unsigned char sendCommandBuffer[100];
+    int sendCommunicationState;
+    unsigned char numberOfSendCommands;
+    unsigned char currentSendCommandIndex;
+    unsigned char sendCommandBuffer[100];
 
-  unsigned char nodeNr;
+    unsigned char nodeNr;
 };
 #endif
