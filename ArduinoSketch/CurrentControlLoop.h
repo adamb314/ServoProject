ce545143de5440942107e20058dd5e9d9f58601c
adam <adamb3.14@gmail.com>
2020-03-20 00:10:22 +0100

    implemented CurrentControlModel

--- ArduinoSketch/CurrentControlLoop.h
+++ ArduinoSketch/CurrentControlLoop.h
@@ -44,4 +44,118 @@ private:
     std::vector<CodeBlocksThread*> threads;
 };
 
+class CurrentControlModel
+{
+public:
+    CurrentControlModel(uint32_t period) :
+        pwmInstance(HBridge4WirePwm::getInstance()),
+        disableLoop(true),
+        brake(true),
+        ref(0),
+        y(0),
+        filteredY(0),
+        filteredPwm(0),
+        u(0),
+        limitedU(0),
+        lastULimited(false)
+    {
+    }
+
+    ~CurrentControlModel()
+    {
+    }
+
+    void setReference(int16_t ref)
+    {
+        disableLoop = false;
+        this->ref = ref;
+    }
+
+    int16_t getLimitedRef()
+    {
+        if (lastULimited)
+        {
+            return y;
+        }
+
+        return ref;
+    }
+
+    void overidePwmDuty(int16_t pwm)
+    {
+        disableLoop = true;
+        brake = false;
+        u = pwm;
+    }
+
+    int16_t getFilteredPwm()
+    {
+        return filteredPwm;
+    }
+
+    void activateBrake()
+    {
+        disableLoop = true;
+        brake = true;
+    }
+
+    int16_t getCurrent()
+    {
+        return filteredY;
+    }
+
+    void run(float vel)
+    {
+        if (disableLoop)
+        {
+            if (brake)
+            {
+                pwmInstance->activateBrake();
+                limitedU = 0;
+            }
+            else
+            {
+                limitedU = pwmInstance->setOutput(u);
+            }
+            y = (pwmToStallCurrent + backEmfCurrent * vel) * limitedU;
+            filteredY = y;
+            filteredPwm = limitedU;
+            return;
+        }
+
+        u = ref / (pwmToStallCurrent + backEmfCurrent * vel);
+
+        limitedU = pwmInstance->setOutput(u);
+
+        y = (pwmToStallCurrent + backEmfCurrent * vel) * limitedU;
+        filteredY = y;
+        filteredPwm = limitedU;
+
+        if (u != limitedU)
+        {
+            lastULimited = true;
+        }
+        else
+        {
+            lastULimited = false;
+        }
+    }
+
+private:
+
+    static constexpr float pwmToStallCurrent = 2.61598722;
+    static constexpr float backEmfCurrent = -0.70435649 * 2 * 3.1415926535897932384626433832795028841972 / 4096.0;
+
+    PwmHandler* pwmInstance;
+    bool disableLoop;
+    bool brake;
+    int16_t ref;
+    int16_t y;
+    int16_t filteredY;
+    int16_t filteredPwm;
+    int16_t u;
+    int16_t limitedU;
+    bool lastULimited;
+};
+
 #endif
